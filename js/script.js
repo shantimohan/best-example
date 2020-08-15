bdac0bc9186c902b56f4d0e0f8062b71b77b03ef
Shantimohan Elchuri <shantimohan@outlook.com>
2020-08-15 08:42:30 -0400

    :art: Replace alert with something better
    Fixes #4

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener(
   "DOMContentLoaded"
   function(event) {
-    alert('The page is loaded and the script ran!')
+    var message = document.getElementById('messge')
+    messge.innerText = "The Script ran!"
   }
 );
